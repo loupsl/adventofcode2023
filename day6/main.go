4ff466bd9238256fad77c00ce877195f28ea6e8f
loupsl <126770426+loupsl@users.noreply.github.com>
2023-12-06 14:41:00 +0100

    Day6 part1

---
+++ day6/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("C:/Users/pelis/Documents/Mines2A/Programminglang/adventofcode2023/day6/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	res := []string{}
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+
+	times := convert_int(strings.Split(strings.Split(res[0], ":        ")[1], "     "))
+	distances := convert_int(strings.Split(strings.Split(res[1], ":   ")[1], "   "))
+
+	fmt.Println(result(times, distances))
+
+}
+
+func convert_int(tab []string) []int {
+	tabint := []int{}
+	for _, val := range tab {
+		value, err := strconv.Atoi(val)
+		if err != nil {
+			fmt.Println("Erreur de conversion")
+			return tabint
+		}
+		tabint = append(tabint, value)
+	}
+	return tabint
+}
+
+// distance if we held the button for "time_heldbutton" in a race that lasts "time"
+func result_distance(time int, time_heldbutton int) int {
+	return time_heldbutton * (time - time_heldbutton)
+}
+
+func count_waystowin(time int, record int) int {
+	count := 0
+	for i := 0; i <= time; i++ {
+		time_heldbutton := i
+		distance := result_distance(time, time_heldbutton)
+		if distance > record {
+			count++
+		}
+	}
+	return count
+}
+
+func result(times []int, distances []int) int {
+	result := 1
+	for i := 0; i < 4; i++ {
+		result = result * count_waystowin(times[i], distances[i])
+	}
+	return result
+}
